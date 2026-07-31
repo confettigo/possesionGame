1bfb39e174b8291923f23dbd20262110ba243ab6
endless-stars66 <opal.lovelace66@gmail.com>
2026-07-31 16:58:07 -0400

    Added titlescreen, basic character and exit block, tilset

---
+++ code/title.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+
+func _on_lvl_1_button_up() -> void:
+	Global.switchScene("res://scenes/levels/tester0.tscn")
+
+
+func _on_lvl_2_button_up() -> void:
+	Global.switchScene("res://scenes/levels/tester_1.tscn")
+	pass # Replace with function body.
