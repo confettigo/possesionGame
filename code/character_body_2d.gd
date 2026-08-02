b1e99ddd486abb59007c54ad6d9df9fce1408748
confettigo <ihostiuk@gmail.com>
2026-08-02 02:09:58 -0400

    added an early player sprite and animations for it, made some adjustments to the tileset and added two new objects to it

--- code/character_body_2d.gd
+++ code/character_body_2d.gd
@@ -1,9 +1,16 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+#do we need jump velocity? - lucie
 
+var animatedSprite
+
+func _ready():
+	#this bit imports the animatedSprite2D that's a child of player so we can control it's animations in the player body
+	#idk if we care about best practices rn but if you think it should be handled via a script on the sprite itself instead of this script using signals or whatever
+	#i could redo it :D - lucie
+	animatedSprite = get_node("AnimatedSprite2D")
 
 func _physics_process(delta: float) -> void:
 
@@ -22,5 +29,19 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.y = move_toward(velocity.y, 0, SPEED)
 	
-
+	
+	#animation code
+	if velocity.x == velocity.y && velocity.y == 0:
+		animatedSprite.play("idle")
+		#this should probably be changed so it plays a different idle animation depending on the direction the player was last moving in...
+		#also it's SUPER inefficiently written code but. it's 2:07 am.... - lucie
+	elif velocity.x < 0:
+		animatedSprite.play("leftFloat")
+	elif velocity.x > 0:
+		animatedSprite.play("rightFloat")
+	elif velocity.y < 0:
+		animatedSprite.play("upFloat")
+	elif velocity.y > 0:
+		animatedSprite.play("downFloat")
+	
 	move_and_slide()
