ac11217db1191775e0c63eae09a4859b8efd6e98
endless-stars66 <opal.lovelace66@gmail.com>
2026-08-02 22:45:20 -0400

    Initial levels 1-3

--- code/title.gd
+++ code/title.gd
@@ -2,11 +2,11 @@ extends Node2D
 
 
 func _on_lvl_1_button_up() -> void:
-	Global.switchScene("res://scenes/levels/tester0.tscn")
+	Global.switchScene("res://scenes/levels/v_2_lvl_1.tscn")
 
 
 func _on_lvl_2_button_up() -> void:
-	Global.switchScene("res://scenes/levels/tester_1.tscn")
+	Global.switchScene("res://scenes/levels/V2Lvl2.tscn")
 	
 func _on_lvl_3_button_up() -> void:
-	Global.switchScene("res://scenes/levels/tester_2.tscn")
+	Global.switchScene("res://scenes/levels/v_2_lvl_3.tscn")
